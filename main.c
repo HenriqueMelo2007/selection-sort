3095938347cf1a2454142a3116c89d0aa6c03724
HenriqueMelo2007 <henriquemelo0983@gmail.com>
2024-02-28 20:05:43 -0300

    feat: new variable included to store the size of the list

--- main.c
+++ main.c
@@ -6,10 +6,23 @@ int main(int argc, char const *argv[])
 {
   setlocale(LC_ALL, "Portuguese_Brazil");
 
+  int size;
   int order;
 
-  printf(" Type: \n 1 - Ascending order \n 2 - Descending order");
-  printf(" Write 8 numbers");
-  
+  printf(" Define how many numbers you want to sort: ");
+  scanf("%i", &size);
+
+  printf(" Type: \n 1 - Ascending order \n 2 - Descending order\n");
+
+  do { scanf("%i", &order); } while ( order != 1 && order != 2 );
+
+  int list[size];
+
+  for (int i = 0; i < size; i++) {
+    printf("Type a number: ");
+    scanf("%i", &list[i]);
+  }
+
+
   return 0;
 }
